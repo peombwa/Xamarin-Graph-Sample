1b01c301a1afdf87d041ac837954fd9e54d1cec4
Peter Wasonga Ombwa <pwombwa@live.com>
2019-05-23 18:48:48 -0700

    Add Xamarin.Android project.

---
+++ Xamarin-Graph-Sample-Android/MainActivity.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Identity.Client;
+using Xamarin_Graph_Sample_Android.Services;
+
+namespace Xamarin_Graph_Sample_Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        GraphService graphService;
+        ProgressBar pbProgess;
+        TextView txtMessage;
+        FloatingActionButton fab;
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            graphService = new GraphService(this);
+
+            fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            pbProgess = FindViewById<ProgressBar>(Resource.Id.pbProgess);
+            txtMessage = FindViewById<TextView>(Resource.Id.txtMessage);
+
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            txtMessage.Text = GetString(Resource.String.txt_downloading_message);
+            pbProgess.Visibility = ViewStates.Visible;
+            fab.Visibility = ViewStates.Invisible;
+
+            Task.Run(async () => {
+                var filesToDownload = await graphService.GetFilesWithAttachmentsAsync();
+
+                List<Task> tasks = new List<Task>();
+                int count = 0;
+                int totalByteCount = 0;
+                SemaphoreSlim semaphore = new SemaphoreSlim(10);
+                foreach (var file in filesToDownload)
+                {
+                    var downloadFileTask = Task.Run(async () =>
+                    {
+                        try
+                        {
+                            await semaphore.WaitAsync();
+
+                            count++;
+                            string message = $"Downloading file {count} of {filesToDownload.Count}.";
+                            System.Diagnostics.Debug.WriteLine(message);
+
+                            RunOnUiThread(() => {
+                                txtMessage.Text = message;
+                            });
+
+                            var fileContent = await graphService.DownloadFileAsync(file);
+
+                            System.Diagnostics.Debug.WriteLine($"Downloaded file {count}.{file.Name} of {fileContent.Length/ 1024} KB size.");
+                            totalByteCount += fileContent.Length;
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
+                        finally
+                        {
+                            semaphore.Release();
+                        }
+                    });
+                    tasks.Add(downloadFileTask);
+                }
+
+                Task.WaitAll(tasks.ToArray());
+
+                RunOnUiThread(() => {
+                    pbProgess.Visibility = ViewStates.Invisible;
+                    fab.Visibility = ViewStates.Visible;
+                    txtMessage.Text = GetString(Resource.String.txt_default_message);
+
+                    View view = (View)sender;
+                    Snackbar.Make(view, $"Downloaded {count} files of total size {totalByteCount / 1048576} MB.", Snackbar.LengthLong)
+                        .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+                });
+            });
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
+
---
+++ Xamarin-Graph-Sample-Android/Services/GraphService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Android.App;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using Xamarin.Android.Net;
+
+namespace Xamarin_Graph_Sample_Android.Services
+{
+    public class GraphService
+    {
+        public string clientID = "{YOUR_CLIENT_ID}"; // Rememeber to sepcify the clientID in your AndroidManifest.xml as well.
+        public string[] scopes = { "Files.ReadWrite.All" };
+        public IPublicClientApplication AuthClientApp { get; private set; }
+        public GraphServiceClient GraphClient { get; set; }
+
+        public GraphService(Activity activity)
+        {
+            AuthClientApp = PublicClientApplicationBuilder.Create(clientID)
+                .WithRedirectUri($"msal{clientID}://auth")
+                .Build();
+
+            var authProvider = new DelegateAuthenticationProvider(async (request) =>
+            {
+                IEnumerable<IAccount> accounts = await AuthClientApp.GetAccountsAsync();
+                AuthenticationResult authResult;
+                try
+                {
+                    authResult = await AuthClientApp.AcquireTokenSilent(scopes, accounts.FirstOrDefault())
+                    .ExecuteAsync();
+                }
+                catch (MsalUiRequiredException)
+                {
+                    authResult = await AuthClientApp.AcquireTokenInteractive(scopes)
+                    .WithParentActivityOrWindow(activity)
+                    .ExecuteAsync();
+                }
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+            });
+
+            // Use AndroidClientHandler as opposed to HttpClientHandler.
+            var innerHandler = new AndroidClientHandler { AllowAutoRedirect = false };
+            var pipeline = GraphClientFactory.CreatePipeline(GraphClientFactory.CreateDefaultHandlers(authProvider), innerHandler);
+
+            GraphClient = new GraphServiceClient(authProvider, new HttpProvider(pipeline, true, new Serializer()));
+        }
+
+        public async Task<IList<DriveItem>> GetFilesWithAttachmentsAsync()
+        {
+            try
+            {
+                List<DriveItem> files = new List<DriveItem>();
+                var driveItems = await GraphClient.Me.Drive.Root.Children.Request().OrderBy("name desc").GetAsync();
+                return driveItems.Where(di => di.File != null).ToList();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Failed to get files to download : {ex.InnerException?.Message}");
+                throw ex;
+            }
+        }
+
+        public async Task<byte[]> DownloadFileAsync(DriveItem item)
+        {
+            try
+            {
+                using (var stream = await GraphClient.Drives[item.ParentReference.DriveId].Items[item.Id].Content.Request().GetAsync())
+                using (var outputStream = new MemoryStream())
+                {
+                    await stream.CopyToAsync(outputStream);
+                    return outputStream.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Failed to download file {item.Id} : {ex.InnerException?.Message}");
+                throw ex;
+            }
+        }
+    }
+}
